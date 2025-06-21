d1ed76226b8ef37c910343ddaafa3b37da4b4806
Faraz <fzzrxx@gmail.com>
2025-06-20 23:03:47 -0400

    Created and implemented a launch file in the workspace.

--- src/arduinobot_py_examples/setup.py
+++ src/arduinobot_py_examples/setup.py
@@ -20,6 +20,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'simple_parameter = arduinobot_py_examples.simple_parameter:main',
+            'simple_publisher = arduinobot_py_examples.simple_publisher:main'
         ],
     },
 )
